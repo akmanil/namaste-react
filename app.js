923ef3922ab75c94033da7ab1ad5b24acd13d982
akmanil <maharanaanil424@gmail.com>
2025-06-06 18:27:10 +0530

    adding parcel file bundler which is used to create a server for react app

---
+++ app.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+document.getElementById("root").innerHTML="Hello React"
+       const heading = React.createElement("h1" , {className: "prop"} , "Hello Anil");
+//        const heading = React.createElement(
+//   "h1", 
+//   { 
+//     style: { 
+//       color: "red",
+//       backgroundColor: "yellow",     // we are also used that code
+//       fontSize: "24px",
+//       padding: "10px"
+//     } 
+//   },
+//   "Hello Anil"
+// );
+       //createRoot() is a React 18+ method that creates a root for your React application
+       const root = ReactDOM.createRoot(document.getElementById("root"));
+       // for render use root.render()
+       root.render(heading)
+  
